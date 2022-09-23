438b74028863db34c3ad5fcc21ae7a06219c5e7d
aldo surya <aldosurya199200@gmail.com>
2022-09-23 20:43:54 +0700

    initial commit

---
+++ test.py
@@ -0,0 +1,5 @@
+def fun1(name, age=20):
+    print(name, age)
+fun1 = ("emma", 25)
+print(fun1)
+        
---
+++ tugas.py
@@ -0,0 +1,15 @@
+class Hewan:
+    nama_latin = hewan
+    def __init__(self, nama, umur):
+        self.nama = nama
+        self. umur = umur
+    def bangun(self):
+        return self
+    def change_nama_latin(change_nama_latin):
+        return change_nama_latin    
+class Kucing:
+    def __init__(channge_nama_latin):
+        pass
+    
+              
+
